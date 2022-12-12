0b0c61a4323a0f2c65f71e58035f39f206a198bd
Speedev <roberto.tg99@gmail.com>
2022-12-12 13:24:11 -0500

    feat: Add axios interceptor

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
+import { AxiosInterceptor } from './interceptors/axios.interceptor'
+
+AxiosInterceptor()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
